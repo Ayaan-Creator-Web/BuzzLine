cbbefdf01d0d38e1cbed487bb8dcb214b2c6ad4b
Ayaan-Creator-Web <ayaan.khalique3@gmail.com>
2025-07-08 18:01:02 +1000

    Update login.js

--- login.js
+++ login.js
@@ -1,54 +1,39 @@
-var email
-
-var password
-
+var email;
+var password;
 var clicked;
-
-var username
-
+var username;
 var savedusername = '';
 
-const verifypath = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+let apps = JSON.parse(localStorage.getItem('apps')) || [{
+    name: 'Google',
+    //url: 'https://eviltester.github.io/TestingApp/apps/iframe-search/iframe-search.html',
+    url: 'https://bing.com/search?q=search'
+}, {
+    name: 'Timely',
+    url: 'https://www.timelypro.online'
+}];
 
-//JSON.parse(localStorage.getItem('users'))
+const verifypath = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 let users = [{
     username: 'Ayaan',
     email: 'ayaan.khalique3@gmail.com',
     password: 'sd',
-    subscription: 'Premium'
 }, {
     username: 'Khalique',
     email: 'khaliquer@gmail.com',
     password: 'Oyster@22',
-    subscription: 'Free'
-}, {
-    username: 'BuzzLine Official',
-    email: 'ayaan.khalique3@gmail.com',
-    password: 'sd',
-    subscription: 'Admin'
-}, {
-    username: 'Iron Woman',
-    email: 'nicezara@gmail.com',
-    password: 'apple',
-    subscription: 'Free'
-}, {
-    username: 'Babu',
-    email: 'muhammadbashir@gmail.com',
-    password: 'hi',
-    subscription: 'Free'
-}];
-/*
-[{
-    username: 'Ayaan',
-    email: 'ayaan@example.com',
-    password: 'student123'
 }, {
     username: 'Wasiullah',
-    email: 'wasiullah@example.com', 
-    password: 'teacher321'
-}]
-*/
+    email: '',
+    password: 'sd'
+}];
+
+document.getElementById('password').addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') signin();
+});
+
+const loginSound = new Audio('../audio/startup.mp3');
 
 async function signin() {
     const password = document.getElementById('password').value;
@@ -68,115 +53,52 @@ async function signin() {
         alert('Please Check Email or Password');
         return;
     }
-    localStorage.setItem('BuzzLineUser', JSON.stringify(user));
+    localStorage.setItem('miniOS-User', JSON.stringify(user));
+    start();
+    // Add the 'unblurred' class to the body
+    document.body.classList.add('unblurred');
 
-    document.querySelector('.all').classList.add('hidden');
-    document.body.innerHTML += '<div id="load"></div>';
-    document.getElementById('load').classList.add('load');
-    // Redirect after a short delay if you want
-    // await delay(1000);
-    window.location.href = "https://ayaan-creator-web.github.io/BuzzLine";
-}
-
-function signup(newId) {
-    email = document.getElementById('email2').value
-    password = document.getElementById('password2').value
-    username = document.getElementById('username2').value
-
-    var newId
-
-    if (users.length == 0) {
-        newId = 0;
-    }
-    else {
-        newId = users.length
-    }
-
-    if (!CheckUserExist(email)) {
-        if (email.match(verifypath)) {
-            if (username && password) {
-                const newuser = {
-                username,
-                email,
-                password,
-                id: newId
-                }
-                users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : []
-                users.push(newuser);
-                alert('Thanks for Signing Up ' + username + '! Please Sign In to Continue')
-                localStorage.setItem('users', JSON.stringify(users));
-
-            }
-            else {
-                alert('Please Fill in Form Completely')
-            }
-        }
-        else {
-            alert('Please Input a Valid Email')
-        }
-    
-    }
-    else {
-        alert('That Email is Already Registered');
-    }
+    // Wait for the transition to complete, then start the next action
+    await delay(8); // Match this delay to your CSS transition duration
 }
 
 function CheckUserExist(email) {
-
-    //fetching updated user from local storage
-    users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : []
-    let doesExist = false
-    //debugger;
+    let doesExist = false;
     users.forEach((check) => {
-       if (check.email == email) {
-        doesExist = true;
-       }
+        if (check.email == email) {
+            doesExist = true;
+        }
     });
     return doesExist;
 }
 
+async function start() {
+    tako.log('MiniOS Started');
+    await loginSound.play();
+    document.body.innerHTML = `
+        <div id="taskbar">
+        </div>
+        <iframe id="app" style="width: 100%; height: 90vh; border: none;"></iframe>
+    `;
+    showApps();
+}
 
-async function forgotPassword() {
-    if (localStorage) {
-        window.location.href="Forgot-Password.html";
-    }
+function showApps() {
+    const taskbar = document.getElementById('taskbar');
+    apps.forEach((app) => {
+        taskbar.innerHTML += `
+            <image style="width: 50px;height: 50px;" src="/images/${(app.name).toLowerCase()}.jpg" id="${app.name}" onclick="'${(app.name)}'.startApp();"></image>
+        `
+    })
+};
+
+String.prototype.startApp = function() {
+    apps.forEach((app) => {
+        if (app.name == this) {
+            if (document.getElementById('app').src == app.url) document.getElementById('app').src = '';
+            else document.getElementById('app').src = app.url;
+        }
+    });
 }
 
 function delay(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
-
-//localStorage.clear()
-//johndoe@example.com
-//teacher321
-//student123
-//0187009@schoolsnet.act.edu.au
-
-/*
-when signed in:
-set local storage for login user
-redirect to new page
-do not allow user to paste url in browser
-allow hompage to only allow users
-localStorage.setItem('xyz', 'login successfully');
-say welcome username in alert message
-*/
-
-function membership() {
-    if (!clicked) {
-        document.querySelector('.membership').innerText = 'Click to hide membership details';
-        clicked = true;
-        document.body.innerHTML += `<table>
-                                        <tr>
-                                            <th></th>
-                                            <th>Free</th>
-                                            <th>Premium</th>
-                                        </tr>
-                                        <tr>
-                                            <td>Access to all buttons</td>
-                                            <td>No</td>
-                                            <td>Yes</td>
-                                    </table>`
-    } else {
-        document.querySelector('.membership').innerText = 'Click to show membership details';
-        clicked = false;
-    }
-}
